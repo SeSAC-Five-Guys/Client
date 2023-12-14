b665b6c5ca1ae793d91daabc9e672f59ad9ca3fe
kkkwp <chxrryda@gmail.com>
2023-12-15 01:53:24 +0900

    💄 style: 테마 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ThemeProvider from './theme';
 import Router from './Router';
 
 export default function App() {
   return (
-    <Router />
+    <ThemeProvider>
+      <Router />
+    </ThemeProvider>
   );
 }
