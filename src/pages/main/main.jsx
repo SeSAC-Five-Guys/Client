30f28ac7f9576de8b4ec308fd82ad212d25cb60a
kkkwp <chxrryda@gmail.com>
2024-01-02 01:35:44 +0900

    ✨ feat: header 추가

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,12 +1,20 @@
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
 
+import Header from '../../components/header';
 import UserInfo from '../../components/userInfo';
 import ItemIcon from '../../components/itemIcon';
 
 export default function Main() {
   return (
     <Container maxWidth="xl">
+      <Header />
+
+      <Typography variant="h4" sx={{ mb: 5 }}>
+        Hi, Welcome back 👋
+      </Typography>
+
       <Grid container spacing={3}>
         <Grid xs={12} md={6} lg={4}>
           <UserInfo
