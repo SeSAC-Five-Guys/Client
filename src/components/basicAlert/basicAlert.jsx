c9050657c147b8c77b3702cbcdb216853647a641
kkkwp <chxrryda@gmail.com>
2024-01-08 13:04:09 +0900

    💄 style: alert title 추가

--- src/components/basicAlert/basicAlert.jsx
+++ src/components/basicAlert/basicAlert.jsx
@@ -1,6 +1,8 @@
 import Fade from '@mui/material/Fade';
 import Alert from '@mui/material/Alert';
 import Dialog from '@mui/material/Dialog';
+import AlertTitle from '@mui/material/AlertTitle';
+import Typography from '@mui/material/Typography';
 
 function BasicAlert({ open, handleClose, severity, message }) {
   return (
@@ -8,13 +10,18 @@ function BasicAlert({ open, handleClose, severity, message }) {
       open={open}
       onClose={handleClose}
       TransitionComponent={Fade}
-      sx={{ height: '200px' }}
+      sx={{ height: '50%' }}
     >
       <Alert
         severity={severity}
         onClose={handleClose}
         sx={{ width: 500, height: 80, alignItems: 'center' }}
       >
+        <AlertTitle>
+          <Typography component="div" fontWeight="bold">
+            {severity.toUpperCase()}
+          </Typography>
+        </AlertTitle>
         {message}
       </Alert>
     </Dialog>
